cc5018d0d9471ab327664ac034f8ee964ba40f36
Ys <4900895+yschua@users.noreply.github.com>
2023-06-25 21:14:58 +1000

    Add heater level setting editor view

---
+++ src/domo/Data/HeaterLevelSetting.cs
@@ -0,0 +1,10 @@
+﻿namespace domo.Data;
+
+public class HeaterLevelSetting
+{
+    public required TimeSpan? InitialDuration { get; set; }
+
+    public required TimeSpan? FinalDuration { get; set; }
+
+    public required TimeSpan? DurationChange { get; set; }
+}
