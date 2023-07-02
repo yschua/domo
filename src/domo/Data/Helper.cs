55e364468e9a7be25b8bfc9748a1f21d4d03709e
Ys <4900895+yschua@users.noreply.github.com>
2023-07-02 22:00:24 +1000

    Add missing file

---
+++ src/domo/Data/Helper.cs
@@ -0,0 +1,8 @@
+﻿namespace domo.Data;
+
+public static class Helper
+{
+    public static TimeSpan Max(TimeSpan val1, TimeSpan val2) => (val1 > val2) ? val1 : val2;
+
+    public static TimeSpan Min(TimeSpan val1, TimeSpan val2) => (val1 < val2) ? val1 : val2;
+}
